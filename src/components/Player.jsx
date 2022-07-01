c6871e93c500682de2c02c7f133514533ddbdcdb
Ayaz <hsinsa758@gmail.com>
2022-07-01 11:52:11 +0300

    added a data file

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,30 +2,31 @@ import React from "react";
 
 function Player() {
   return (
-    <div className="player">
-      <div className="time-control">
-        <p className="start-time"></p>
-        <input type="range" />
-        <p className="end-time"></p>
+    <div className="flex flex-col items-center gap-10 min-h-[20vh]">
+      <div className="lg:w-2/3 w-11/12 flex flex-col">
+        <div className="flex w-auto justify-between">
+            <p className="">01:00</p>
+            <p className="">02:35</p>
+        </div>
+        <input className="w-full" type="range" />
       </div>
-      <div className="play-control">
-
-        <span className="back">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <div className="flex gap-16">
+        <span className="">
+          <svg className="w-12 lg:w-14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M13 5.9296875L6.9296875 12L13 18.070312L14.5 16.570312L9.9296875 12L14.5 7.4296875L13 5.9296875 z" />
           </svg>
         </span>
-        <span className="play">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <span className="">
+          <svg className="w-12 lg:w-14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M8,5v14l11-7L8,5z" />
           </svg>
         </span>
-        <span className="forward">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <span className="">
+          <svg className="w-12 lg:w-14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M10 5.9296875L8.5 7.4296875L13.070312 12L8.5 16.570312L10 18.070312L16.070312 12L10 5.9296875 z" />
           </svg>
         </span>
-        
+
       </div>
     </div>
   );
--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Song() {
-  return <div className="song-container"></div>;
+  return <div className="song-container min-h-[60vh] flex flex-col items-center justify-center">
+    <span>picture</span>
+    <span>song name</span>
+    <span>artist</span>
+  </div>;
 }
 
 export default Song;
